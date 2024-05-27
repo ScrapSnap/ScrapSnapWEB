fa8add7b5735cb755766a448595b40b1b9b83604
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-05-27 15:37:34 +0200

    Implemented creating and editing users

---
+++ src/views/pages/users/EditUser.vue
@@ -0,0 +1,115 @@
+<template>
+  <Dialog v-model:visible="visible" modal header="Edit User" :draggable="false" :style="{ width: '40rem' }" :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
+    <div class="flex align-items-center gap-3 mb-3">
+      <label for="type" class="font-semibold w-6rem">Role</label>
+      <Dropdown v-model="selectedRole" :options="roles" optionLabel="name" placeholder="Select a Role" class="w-full md:w-14rem" style="z-index: 1000;"  />
+    </div>
+    <div class="flex align-items-center gap-3 mb-3">
+      <label for="firstname" class="font-semibold w-6rem">Name</label>
+      <div class="flex flex-column gap-2">
+        <InputText v-model="firstname" class="flex-auto" />
+      </div>
+    </div>
+    <div class="flex align-items-center gap-3 mb-3">
+      <label for="lastname" class="font-semibold w-6rem">Lastname</label>
+      <div class="flex flex-column gap-2">
+        <InputText v-model="lastname" class="flex-auto" />
+      </div>
+    </div>
+    <div class="flex align-items-center gap-3 mb-3">
+      <label for="email" class="font-semibold w-6rem">Email</label>
+      <div class="flex flex-column gap-2">
+        <InputText v-model="email" class="flex-auto" />
+      </div>
+    </div>
+    <div class="flex align-items-center gap-3 mb-3">
+      <label for="password" class="font-semibold w-6rem">Password</label>
+      <div class="flex flex-column gap-2">
+        <InputText v-model="password" type="password" class="flex-auto" />
+        <small id="username-help">Empty password means password will not be changed.</small>
+      </div>
+    </div>
+    <div class="flex justify-content-end gap-2">
+      <Button type="button" label="Cancel" severity="secondary" @click="visible = false"></Button>
+      <Button type="button" label="Save" @click="updateUser"></Button>
+    </div>
+  </Dialog>
+
+  <Toast />
+</template>
+
+<script setup>
+import {onMounted, ref, watch} from "vue";
+import axios from "@/axios";
+import { useToast } from "primevue/usetoast";
+
+const toast = useToast();
+
+const visible = ref(false);
+const firstname = ref('');
+const lastname = ref('');
+const email = ref('');
+const password = ref('');
+const selectedRole = ref();
+const roles = ref([]);
+
+const emit = defineEmits(['added']);
+
+const loadRoles = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/roles`);
+    roles.value = response.data;
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 });
+  }
+}
+
+const updateUser = async () => {
+  if (!selectedRole.value) {
+    toast.add({ severity: 'info', summary: 'Info', detail: 'Please select a role', life: 3000 });
+    return;
+  }
+
+  if (!firstname.value || !lastname.value || !email.value) {
+    toast.add({ severity: 'info', summary: 'Info', detail: 'Please fill required fields', life: 3000 });
+    return;
+  }
+
+  try {
+    const response = await axios.put(`${import.meta.env.VITE_API_BASE_URL}/users`, {
+      firstname: firstname.value,
+      lastname: lastname.value,
+      email: email.value,
+      password: password.value ? password.value : '',
+      roleId: selectedRole.value.id
+    });
+
+    if (!response.data) {
+      toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 });
+      return;
+    }
+
+    toast.add({ severity: 'success', summary: 'Success', detail: 'Successfully added', life: 3000 });
+    visible.value = false;
+    emit('updated')
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 })
+  }
+}
+
+const showDialog = (user) => {
+  firstname.value = user.firstname;
+  lastname.value = user.lastname;
+  email.value = user.email;
+  selectedRole.value = roles.value.find(role => role.id === user.roleId);
+  visible.value = true;
+}
+
+onMounted(() => {
+  loadRoles();
+})
+
+defineExpose({
+  showDialog
+})
+</script>
---
+++ src/views/pages/users/Users.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="card">
+    <h5>Users</h5>
+    <p>Use this page to start from scratch and place your custom content.</p>
+
+    <div class="w-full flex justify-content-end mb-3">
+      <Button label="New User" icon="pi pi-plus" @click="showAddUserDialog" />
+    </div>
+
+    <DataTable :value="users" stripedRows>
+      <Column field="firstname" header="Name" sortable>
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <span v-else>{{ data.firstname }}</span>
+        </template>
+      </Column>
+      <Column field="lastname" header="Lastname" sortable>
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <span v-else>{{ data.lastname }}</span>
+        </template>
+      </Column>
+      <Column field="email" header="Email" sortable>
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <span v-else>{{ data.email }}</span>
+        </template>
+      </Column>
+      <Column field="createdAt" header="Date Created" sortable>
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <span v-else>{{ formatDate(data.createdAt) }}</span>
+        </template>
+      </Column>
+      <Column field="updatedAt" header="Date Updated" sortable>
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <span v-else>{{ formatDate(data.updatedAt) }}</span>
+        </template>
+      </Column>
+      <Column header="Actions">
+        <template #body="{ data }">
+          <Skeleton v-if="loading"></Skeleton>
+          <Button icon="pi pi-pencil" @click="showEditUserDialog(data)" severity="warning" class="mr-1" aria-label="Info" />
+          <Button icon="pi pi-trash" @click="deleteUser(data)" severity="danger" class="ml-1" aria-label="Delete" />
+        </template>
+      </Column>
+    </DataTable>
+
+    <Toast />
+    <AddUser ref="addUserDialog" @added="loadUsers" />
+    <EditUser ref="editUserDialog" @updated="loadUsers" />
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue";
+import AddUser from "@/views/pages/users/AddUser.vue";
+import { useToast } from "primevue/usetoast";
+import axios from "@/axios";
+import EditUser from "@/views/pages/users/EditUser.vue";
+
+const toast = useToast();
+
+const loading = ref(true);
+const users = ref([]);
+const addUserDialog = ref();
+const editUserDialog = ref();
+
+onMounted(() => {
+  loadUsers();
+});
+
+const loadUsers = async () => {
+  loading.value = true;
+
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/users`);
+
+    if (!response.data) {
+      toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 });
+      return;
+    }
+
+    console.log(response.data);
+    users.value = response.data;
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 })
+  } finally {
+    loading.value = false;
+  }
+}
+
+const deleteUser = async (data) => {
+  try {
+    console.log(data);
+    const response = await axios.delete(`${import.meta.env.VITE_API_BASE_URL}/users/${data._id}`);
+
+    if (!response.data) {
+      toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 });
+      return;
+    }
+
+    toast.add({ severity: 'success', summary: 'Success Message', detail: 'Successfully deleted', life: 3000 });
+    loadUsers();
+  } catch (error) {
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Something went wrong...', life: 3000 })
+  }
+}
+
+const formatDate = (dateString) => {
+  const date = new Date(dateString);
+  return date.toLocaleDateString() + " " + date.toLocaleTimeString();
+}
+
+const showAddUserDialog = () => {
+  addUserDialog.value.showDialog();
+}
+
+const showEditUserDialog = (data) => {
+  console.log(data);
+  editUserDialog.value.showDialog(data);
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>

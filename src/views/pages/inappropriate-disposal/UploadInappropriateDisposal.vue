fc2d7fc015ca91a7cd35ffac08c703604e226c1a
blaz-cerpnjak <blaz003cerpnjak@gmail.com>
2024-05-27 08:07:01 +0200

    Added page for uplading inappropriate disposals

---
+++ src/views/pages/inappropriate-disposal/UploadInappropriateDisposal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="grid">
+    <div class="col-12">
+      <div class="card">
+        <h5>Upload Inappropriate Disposal</h5>
+        <FileUpload name="demo[]" @uploader="onUpload" :multiple="true" accept="image/*" :maxFileSize="1000000" customUpload />
+      </div>
+    </div>
+  </div>
+  <Toast />
+</template>
+<script setup>
+import { useToast } from 'primevue/usetoast';
+
+const toast = useToast();
+
+const onUpload = (event) => {
+  const uploadedFiles = event.files;
+  console.log(uploadedFiles);
+  toast.add({ severity: 'success', summary: 'Success', detail: 'File Uploaded', life: 3000 });
+};
+</script>
